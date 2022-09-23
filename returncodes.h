8c707f6ff5672324cbcba2528c447e696d06a54b
bigguccimts <mtsdurica@gmail.com>
2022-09-23 17:11:32 +0200

    Started library with return codes

---
+++ returncodes.h
@@ -0,0 +1,20 @@
+/**
+ * @brief Definitions of error codes
+ */
+
+#ifndef RETURNCODES_H
+#define RETURNCODES_H
+
+#define ALL_GOOD 0            // No errors
+#define LEX_ANALYSIS_ERR 1    // Lexical analysis error
+#define SYNTAX_ANALYSIS_ERR 2 // Wrong code syntax, missing header etc.
+#define UNDEF_FUNC_ERR 3      // Undefined function, redefinition of a function
+#define UNDEF_PARAM_ERR 4     // Wrong amount/type of parameters in function call
+#define UNDEF_RETURN_ERR 4    // Wrong type of return value
+#define UNDEF_VAR_ERR 5       // Undefined variable
+#define WRONG_RET_ERR 6       // Missing/redundant expression in return
+#define TYPE_COMP_ERR 7       // Error in type compatibility in arithmetic, string and relation expressions
+#define SEMANTIC_OTHER_ERR 8  // Other semantic errors
+#define INTERNAL_ERR 99       // Internal error ie. memory allocation error
+
+#endif
