8c9cfe72e312f0aa9cb3a0e1d608dad7a0adac15
Matúš Ďurica <xduric06@stud.fit.vutbr.cz>
2022-11-29 21:11:12 +0100

    Create datatypes.h

---
+++ datatypes.h
@@ -0,0 +1,28 @@
+/**
+ * Implementace překladače imperativního jazyka IFJ22
+ *
+ * @file datatypes.h
+ * @brief File containing data types enums
+ *
+ * @author Matúš Ďurica (xduric06)
+ * @author Ivan Mahút (xmahut01)
+ * @author Dušan Slúka (xsluka00)
+ * @author Gabriela Paganíková (xpagan00)
+ */
+
+#ifndef DATATYPES_H
+#define DATATYPES_H
+
+/**
+ * @brief Contains data types in enums, just for ease of use
+ *
+ */
+typedef enum
+{
+    INT_T,
+    FLOAT_T,
+    STRING_T,
+    VOID_T
+} Data_type;
+
+#endif // DATATYPES_H
